f89276fadc7077e49f3534c8f309952e90844de6
amandayusnita <115141163+amandayusnita@users.noreply.github.com>
2023-05-20 13:14:07 +0700

    Initial commit

---
+++ src/pesanerror.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class pesanerror {
+    public static void main(String[] args) {
+        int angka = 6;
+        int hasil = angka/0;
+        System.out.println(hasil);
+     }
+}
+
+
